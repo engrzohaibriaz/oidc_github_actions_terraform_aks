f6e687c7aa5970e1d3ba820cb8540144ad79099e
engrzohaibriaz <engr.zohaib.riaz@outlook.com>
2026-02-20 05:54:49 +0000

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,5 +1,8 @@
 
 provider "azurerm" {
-  resource_provider_registrations = "none"
+  #resource_provider_registrations = "none" #for 4.0
+  
+# Use this for AzureRM versions older than 4.0
+  skip_provider_registration = true
   features {}
 }
